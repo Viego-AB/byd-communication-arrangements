520379478f9aa48e2dae4acee6875de66978d828
Carl Önnheim <carl.onnheim@viego.se>
2023-11-23 13:43:31 +0100

    Send back request object response

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
+const {parse, stringify, toJSON, fromJSON} = require('flatted');
 const express = require('express');
 const app = express();
 
-// Log the full request, note that this is a security issue - the purpose of this is to trace the security details, do not use with live environments
+// Log and return the full request, note that this is a security issue - the purpose of this is to trace the security details, do not use with live environments
 app.get('/test', function (req, res) {
     console.log(req);
-    res.send('Hello World!');
+    res.json(toJSON(req));
 });
 
 const port = process.env.PORT || 3000;
